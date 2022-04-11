7c3aad30406756927a3ccbebada70154d4aba381
lurensia <woder318@gmail.com>
2022-04-11 10:11:55 +0900

    head,foot 생성

---
+++ spring_lab/blogv3/src/main/java/site/metacoding/blogv3/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package site.metacoding.blogv3.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity // 해당 파일로 시큐리티가 활성화
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    // 인증 설정하는 메서드
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // super.configure(http);
+        http.csrf().disable(); // 이거 안하면 postman 테스트 못함.
+        http.authorizeRequests()
+                .antMatchers("/s/**").authenticated()
+                .anyRequest().permitAll()
+                .and()
+                .formLogin()
+                .loginPage("/loginForm")
+                .defaultSuccessUrl("/");
+    }
+}
---
+++ spring_lab/blogv3/src/main/java/site/metacoding/blogv3/domain/user/User.java
@@ -0,0 +1,39 @@
+package site.metacoding.blogv3.domain.user;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@EntityListeners(AuditingEntityListener.class) // 이 부분 추가
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(length = 20, nullable = false, unique = true)
+    private String username;
+
+    // 1234 -> SHA256(해시 알고리즘) -> AB4539GDUF3AE -> 이렇게 안하면 시큐리티 거부
+    @Column(length = 20, nullable = false)
+    private String password;
+
+    @Column(length = 60, nullable = false)
+    private String email;
+
+    @CreatedDate // insert 할때만 동작
+    private LocalDateTime createDate;
+    @LastModifiedDate // update 할때만 동작
+    private LocalDateTime updateDate;
+
+}
